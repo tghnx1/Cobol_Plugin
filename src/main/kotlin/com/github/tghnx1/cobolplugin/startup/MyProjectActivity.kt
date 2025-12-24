330bb7678145b1e2ed1b3b6c6011ad5fed08b187
GitHub Action <action@github.com>
2025-12-24 20:19:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/tghnx1/cobolplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/tghnx1/cobolplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.tghnx1.cobolplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
