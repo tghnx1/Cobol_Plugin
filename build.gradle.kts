2e948b30a5f38c417ce3702a3ca8ba32d17c2514
tghnx1 <kokorevm2012@gmail.com>
2026-01-01 16:22:21 +0100

    restored the build.gradle.kts file from the template due to the runtime error

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,8 +9,6 @@ plugins {
     alias(libs.plugins.changelog) // Gradle Changelog Plugin
     alias(libs.plugins.qodana) // Gradle Qodana Plugin
     alias(libs.plugins.kover) // Gradle Kover Plugin
-    id("org.jetbrains.grammarkit") version "2023.3.0.1"
-
 }
 
 sourceSets {
